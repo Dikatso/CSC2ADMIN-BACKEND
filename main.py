e4ee850b5f3cf792f86355ca767cc17d7e12b25c
Botshxlo <Botshxlo.dev@gmail.com>
2022-09-06 02:29:15 +0200

    fix: configure changes to serve origin requests

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import emoji
 from typing import Union
 from src.prisma import prisma
 from src.apis import apis
 from fastapi import FastAPI
 from prisma import Prisma
 from firebase_admin import credentials, initialize_app, storage
+from fastapi.middleware.cors import CORSMiddleware
 
 def initialiseFileStorage():
     cred = credentials.Certificate("serviceAccountKey.json")
@@ -14,13 +16,31 @@ db = Prisma(auto_register=True)
 app = FastAPI()
 app.include_router(apis, prefix="/apis")
 
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.on_event("startup")
 async def startup():
-    print("initialising fileStorage...")
+    print("initialising server...🏗")
+    print("connecting to firebase storage...🔥")
+    print("connected successfully ✅")
     initialiseFileStorage()
-    print("connecting to db...")
+    print("connecting to prisma...💠")
+    print("connected successfully ✅")
     await prisma.connect()
+    print("connecting to db...📙")
+    print("connected successfully ✅")
     await db.connect()
+    print("server initialised & ready to serve 🚀")
 
 @app.on_event("shutdown")
 async def shutdown():
